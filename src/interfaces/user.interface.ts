57ca0c071f400a2d9c8c7fd108492f6d2422222c
me-lucas-al <me.lucasalmeida@gmail.com>
2025-06-25 14:46:58 -0300

    finalize backend

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -15,5 +15,4 @@ export interface UserCreate{
 export interface UserRepository{
     create(data: UserCreate): Promise<User>
     findByEmail(email: string): Promise<User | null>
-    updateContact(data: User): Promise<User>
 }
