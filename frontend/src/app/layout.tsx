21c2c75d6c32f2186436cc1343ada9212a0b7a5f
me-lucas-al <me.lucasalmeida.com>
2025-07-16 22:53:22 -0300

    update favicon and title

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,14 @@
 import { AuthProvider } from '@/contexts/AuthContext';
 import '../styles/globals.css'; 
+
+export const metadata = {
+  title: 'Agenda de Contatos',
+  description: 'Gerencie seus contatos de forma simples e eficiente.',
+  icons: {
+    icon: '/favicon.png'
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
